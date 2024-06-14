8c1a848caea193da5dbb086b9ad860cbaeb67cc9
Burak Karaman <karaman.buraak@gmail.com>
2024-06-14 12:12:29 +0200

    Update blogController.js and blogRoute.js to add CRUD operations

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,49 +1,84 @@
 //*Blog Controller
-
+require("express-async-errors")
 const { BlogPost } = require("../models/blogModel");
 
 module.exports.BlogPostController = {
-  list: async (req, res) => {
-    const data = await BlogPost.find();
+    list: async (req, res) => {
+        // const data = await BlogPost.find({ published: false });
+        const data = await BlogPost.find();
 
-    res.status(200).send({
-      error: false,
-      blogs: data,
-    });
-  },
-  create: async (req, res) => {
-    const data = await BlogPost.create(req.body);
+        res.status(200).send({
+            error: false,
+            blogs: data,
+        });
+    },
+    create: async (req, res) => {
+        const data = await BlogPost.create(req.body);
 
-    res.status(201).send({
-      error: false,
-      blog: data,
-    });
-  },
-  createMany: async (req, res) => {
-    const data = await BlogPost.insertMany(req.body.blogs); //* Çoklu veri create etmek için kullanılan yöntem
-    //* çoklu veri gönderilirken veriyi json formatında gönderiyoruz:
-    //     {
-    //         "blogs": [
-    //     {
-    //       "title": "Blog Title 7",
-    //       "content": "Blog Content 7",
-    //       "published": false
-    //     },
-    //     {
-    //       "title": "Blog Title 8",
-    //       "content": "Blog Content 8",
-    //       "published": false
-    //     },
-    //     {
-    //       "title": "Blog Title 9",
-    //       "content": "Blog Content 9",
-    //       "published": false
-    //     }
-    //   ]
-    // }
-    res.status(201).send({
-      error: false,
-      blog: data,
-    });
-  },
+        res.status(201).send({
+            error: false,
+            blog: data,
+        });
+    },
+    read: async (req, res) => {
+        // const data = await BlogPost.findById(req.params.id); //* sadece id secenegini kabul eder
+        // const data = await BlogPost.findOne({ published: false }); //* ilk false olan blogu getirir
+        const data = await BlogPost.findOne({ _id: req.params.id }); //* diger secenekleri de kabul eder
+
+        res.status(200).send({
+            error: false,
+            blog: data,
+        });
+    },
+    update: async (req, res) => {
+        // const data = await BlogPost.findByIdAndUpdate(req.params.id, req.body, { new: true }); // { new: true } => return new data
+        const data = await BlogPost.updateOne({ _id: req.params.id }, req.body) //* datayı döndürmez yaptığı işlemin özetini döner. O nedenle bu yöntemde newData şeklinde sorgu yazıp güncellenmiş halini gönderebiliriz
+        res.status(202).send({
+            error: false,
+            blog: data,
+            newData: await BlogPost.findOne({ _id: req.params.id })
+        });
+    },
+
+    delete: async (req, res) => {
+        const data = await BlogPost.findByIdAndDelete(req.params.id)
+        if (data) {
+            // res.sendStatus(204)
+            res.status(200).send({
+                error: false,
+                message: "Blog post successfully deleted",
+                deletedData: data
+            })
+        } else {
+            res.sendStatus(404)
+        }
+    },
+
+    createMany: async (req, res) => {
+        const data = await BlogPost.insertMany(req.body.blogs); //* Çoklu veri create etmek için kullanılan yöntem
+        //* çoklu veri gönderilirken veriyi json formatında gönderiyoruz:
+        //     {
+        //         "blogs": [
+        //     {
+        //       "title": "Blog Title 7",
+        //       "content": "Blog Content 7",
+        //       "published": false
+        //     },
+        //     {
+        //       "title": "Blog Title 8",
+        //       "content": "Blog Content 8",
+        //       "published": false
+        //     },
+        //     {
+        //       "title": "Blog Title 9",
+        //       "content": "Blog Content 9",
+        //       "published": false
+        //     }
+        //   ]
+        // }
+        res.status(201).send({
+            error: false,
+            blog: data,
+        });
+    },
 };
--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -5,9 +5,15 @@ const router = require("express").Router();
 const { BlogPostController } = require("../controllers/blogController");
 
 router
-  .route("/post")
-  .get(BlogPostController.list)
-  .post(BlogPostController.create);
+    .route("/post")
+    .get(BlogPostController.list)
+    .post(BlogPostController.create);
+router
+    .route("/post/:id")
+    .get(BlogPostController.read)
+    .put(BlogPostController.update)
+    .delete(BlogPostController.delete)
+
 router.route("/post/createMany").post(BlogPostController.createMany);
 
 module.exports = router;
