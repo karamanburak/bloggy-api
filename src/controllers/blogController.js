34a6e3f3e1d01af3e1e887c5c2b184c1077c1c02
Burak Karaman <karaman.buraak@gmail.com>
2024-06-27 10:36:59 +0200

    Add search and sort functionality to BlogPostController

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -61,6 +61,8 @@ module.exports.BlogPostController = {
 
     //* Searching => gelen ifaade içerisinde geçiyor mu geçmiyor mu
     //https://www.mongodb.com/docs/manual/reference/operator/query/regex/
+    // URL?search[key1]=value1&search[key2]=value2 => url array parameter
+
     const search = req.query?.search || {};
     console.log("search: ", search);
     //* { title: 'Testuser1', content: 'Testuser' } => { title: {$regex:'Testuser1'}, content:{ $regex: 'Testuser'} }
@@ -70,12 +72,21 @@ module.exports.BlogPostController = {
     }
     console.log("search2: ", search);
 
+    //? Sorting
+    // https://mongoosejs.com/docs/api/query.html#Query.prototype.sort()
+    // URL?sort[key1]=value1&sort[key2]=value2 => url array parameter
+    // 1:A-Z - -1:desc: Z-A => deprecated
+    // asc: A-Z - desc: Z-A
+    const sort = req.query?.sort || {};
+
     // const data = await BlogPost.find({}) = BlogPost.find()
     // const data = await BlogPost.find(filter);
     // const data = await BlogPost.find({filter,search}); => {filter:{ userId: '667d10dc03839026052691ab', published: '0' },search:{ title: { '$regex': 'Testuser1' }, content: { '$regex': 'Testuser' } }} // wrong
     // const [a,b,...x] = [12,13,56,6455,456] => rest
     // function(a,...x) => rest
-    const data = await BlogPost.find({ ...filter, ...search }); // spread
+    // const data = await BlogPost.find({ ...filter, ...search }); // spread => yayma
+    const data = await BlogPost.find({ ...filter, ...search }).sort(sort);
+
     // const data = await BlogPost.find({ published: true, }).populate(
     //   "blogCategoryId",
     //   "name -_id"
